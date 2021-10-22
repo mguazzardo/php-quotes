a918e2a31ceda9e22f718cca60d9e1127502585e
mguazzardo <mguazzardo76@gmail.com>
2021-10-21 22:48:31 -0300

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE quote(id int, msg varchar(255));
+
+INSERT INTO quote values(1,'Los viernes no se deploya');
+INSERT INTO quote values(2,'Los que no se equivocan, es por que no hacen nada');
+INSERT INTO quote values(3,'Es solo rock and roll, pero me gusta');
+INSERT INTO quote values(4,'Unix Rulez, DevOps are the best');
