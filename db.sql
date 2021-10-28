a8d341a03571efc5a55ab3cdad4b524530ca9e87
mguazzardo <mguazzardo76@gmail.com>
2021-10-28 18:23:01 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -4,3 +4,7 @@ INSERT INTO quote values(1,'Los viernes no se deploya');
 INSERT INTO quote values(2,'Los que no se equivocan, es por que no hacen nada');
 INSERT INTO quote values(3,'Es solo rock and roll, pero me gusta');
 INSERT INTO quote values(4,'Unix Rulez, DevOps are the best');
+INSERT INTO quote values(5,'Algun dia Demis va a instalar el ocp');
+INSERT INTO quote values(6,'Si laburas en Argentina , el estado es socio');
+INSERT INTO quote values(7,'Si no lo sabe Naguer, no lo sabe nadie');
+INSERT INTO quote values(8,'Los devops somos capos);
